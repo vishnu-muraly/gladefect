15155421945d68667560c0f88041961550c8bcbb
Vishnu Muraly <31529824+vishnumurly@users.noreply.github.com>
2020-03-27 09:10:09 +0530

    updated port number

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 from scripts import app
 
 if __name__ == '__main__':
+    # decide what port to run the app in
+    port = int(os.environ.get('PORT', 8000))
+    # run the app locally on the givn port
+    app.run(host='0.0.0.0', port=port)
+    # optional if we want to run in debugging mode
     app.run(debug=True)
